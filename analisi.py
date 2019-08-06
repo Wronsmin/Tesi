c017c6e905ac0fb26fd34241f2435cbd0e31181e
BleakHeart <cosmin.marin19@gmail.com>
2019-08-06 10:40:05 +0200

    little changes

--- analisi.py
+++ analisi.py
@@ -9,11 +9,14 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True)
 
-def f(x, a, b, c):
+def retta(x, d, e):
+    return d*x + e
+
+def parabola(x, a, b, c):
     return a * x ** 2 + b * x + c
 
 def vertice(array):
-    return np.divide(-popt[1], 2.*popt[0]), np.divide(-np.power(array[1],2.), 4.*array[0]) + array[2]
+    return np.divide(-array[1], 2.*array[0]), np.divide(-np.power(array[1],2.), 4.*array[0]) + array[2]
 
 def find_nearest(array, value):
     idx = np.argmin(_ for _ in array if i > value)
@@ -24,11 +27,12 @@ def Maxima(time, data):
         maximum = np.argmax(i)
         xdata = [0, 1, 2]
         if maximum == 1:
-            popt, _ = curve_fit(f, xdata, i[maximum-1:maximum+2])
+            popt, _ = curve_fit(parabola, xdata, i[maximum-1:maximum+2])
+            tmax, Emax = vertice(popt)
+            t_needed, E_needed = find_nearest( i, Emax / 2.0)
+
         else:
             pass
-        tmax, Emax = vertice(popt)
-        t_needed, E_needed = find_nearest( i, Emax / 2.0)
 
 def lettura_file(files):
     data = pd.read_csv(files, sep=' ')
@@ -50,16 +54,5 @@ if __name__ == '__main__':
 
     tmp = 'peak_finder_dump_fifo_1ch_lkrl0-fe-1a01_Thu__08_Sep_2016_15-28-46.csv'
     timestamp, data = lettura_file(tmp)
-    maximum = np.argmax(data[0])
-    if maximum == 1:
-        xdata = [0, 1, 2]; xdata = np.asarray(xdata)
-        popt, _ = curve_fit(f, xdata, data[0][0:3])
-    else:
-        pass
-    tmax, Emax = vertice(popt)
-    t_needed, E_needed = find_nearest(data[0], Emax / 2.0)
-    plot(data[0])
-    print(tmax, Emax, t_needed, E_needed)
-    xdata = np.linspace(0,6,50)
-    plot(xdata, f(xdata, *popt))
-    show()
+    Maxima(timestamp, data)
+
