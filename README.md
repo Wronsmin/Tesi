f3b46a976fd317e04224b8f6187e4ddc37e8cccc
BleakHeart <cosmin.marin19@gmail.com>
2019-11-09 12:32:36 +0100

    x

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1- Successivamente devo fare le conversioni tra le energie espresse in bit e il tempo.
+
 2- Ancora più tardi una volta perfezionato il programma posso scrivere la parte di parallelizzazione per analizzare tutti i file contemporaneamente.
+
 3- Scrivere tutta la parte statistica, dividere in bin l'enegia e graficare la distribuzione; capire il grafico a intermittenza del tempo etc 
--- analisi.py
+++ analisi.py
@@ -1,16 +1,19 @@
 from matplotlib.pyplot import *
+import os
 from strumenti import *
 
 np.set_printoptions(suppress=True)
 
-if __name__ == '__main__':
-    #working = os.path.realpath('/run/media/wronsmin/Storage/Dataset/160908/Peak_Finder')
-    #list_path = []
-    #for i in os.listdir(working):
-    #    list_path.append(working + '/' + i)
+#working = os.path.realpath('/run/media/wronsmin/Storage/Dataset/160908/Peak_Finder')
+#list_path = []
+#for i in os.listdir(working):
+#    list_path.append(working + '/' + i)
 
-    tmp = 'peak_finder_dump_fifo_1ch_lkrl0-fe-1a01_Thu__08_Sep_2016_15-28-46.csv'
-    timestamp, data = lettura_file(tmp)
-    tempi, energie = Maxima(timestamp, data)
-    plot(energie)
-    show()
+tmp = 'peak_finder_dump_fifo_1ch_lkrl0-fe-1a01_Thu__08_Sep_2016_15-28-46.csv'
+timestamp, data = lettura_file(tmp)
+tempi, energie = Maxima(timestamp, data)
+title('Energia vs $\Delta t$')
+ylabel('Energia')
+xlabel('$\Delta$ t')
+scatter(tempi, energie, s=3)
+savefig('energia_vs_tempo.png')
