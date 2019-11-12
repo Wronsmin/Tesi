53ebe8b35098e1b62c188c0e093c39319578f331
BleakHeart <cosmin.marin19@gmail.com>
2019-11-12 12:35:34 +0100

    boh

--- analisi.py
+++ analisi.py
@@ -3,17 +3,45 @@ import os
 from strumenti import *
 
 np.set_printoptions(suppress=True)
+immagini='/mnt/c/Users/cosmi/Desktop/Python/Tesi/Immagini'
+working = os.path.realpath('/mnt/c/Users/cosmi/Desktop/Python/Tesi/Dataset/160908/Peak_Finder')
+list_path = []
+total_data = 0
+energie, tempi = [], []
 
-#working = os.path.realpath('/run/media/wronsmin/Storage/Dataset/160908/Peak_Finder')
-#list_path = []
-#for i in os.listdir(working):
-#    list_path.append(working + '/' + i)
+for i in os.listdir(working):
+    #list_path.append(working + '/' + i)
+    timestamp, data = lettura_file(working + '/' + i)
+    a, b = Maxima(timestamp, data)
+    energie, tempi = energie + b, tempi + a
+    total_data += len(timestamp)
 
-tmp = 'peak_finder_dump_fifo_1ch_lkrl0-fe-1a01_Thu__08_Sep_2016_15-28-46.csv'
-timestamp, data = lettura_file(tmp)
-tempi, energie = Maxima(timestamp, data)
 title('Energia vs $\Delta t$')
 ylabel('Energia')
 xlabel('$\Delta$ t')
 scatter(tempi, energie, s=3)
-savefig('energia_vs_tempo.png')
+savefig(immagini + '/energia_vs_tempo.png')
+
+title('Plot Energie')
+ylabel('Energia')
+xlabel('# evento')
+plot(energie)
+savefig(immagini + '/energie.png')
+
+title('Plot Tempi')
+ylabel('$\Delta$t')
+xlabel('# evento')
+plot(tempi)
+savefig(immagini + '/tempi.png')
+
+title('Istogramma Energie')
+ylabel('Conteggi')
+xlabel('Energia')
+hist(energie, bins='auto')
+savefig(immagini + '/hist_energie.png')
+
+title('Istogramma Tempi')
+ylabel('Conteggi')
+xlabel('$\Delta$t')
+hist(tempi, bins='auto')
+savefig(immagini + '/hist_tempi.png')
