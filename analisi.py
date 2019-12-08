f404089678bf9cd62197106f6e5a51228b9d6bf0
BleakHeart <cosmin.marin19@gmail.com>
2019-12-08 09:32:58 +0100

    Modificato

--- analisi.py
+++ analisi.py
@@ -3,49 +3,54 @@ import os
 from strumenti import *
 
 np.set_printoptions(suppress=True)
+
 immagini='/mnt/c/Users/cosmi/Desktop/Python/Tesi/Immagini'
-working = os.path.realpath('/mnt/c/Users/cosmi/Desktop/Python/Tesi/Dataset/160908/Peak_Finder')
+working_linux = os.path.realpath('/mnt/c/Users/cosmi/Desktop/Python/Tesi/Dataset/160908/Peak_Finder')
 list_path = []
 total_data = 0
+timestamp, data = [], []
 energie, tempi = [], []
 
 for i in os.listdir(working):
-    #list_path.append(working + '/' + i)
-    timestamp, data = lettura_file(working + '/' + i)
-    a, b = Maxima(timestamp, data)
-    energie, tempi = energie + b, tempi + a
+    timestamp, data += lettura_file(working + '/' + i)
 
-title('Energia vs $\Delta t$')
-ylabel('Energia')
-xlabel('$\Delta$ t')
-scatter(tempi, energie, s=3)
-savefig(immagini + '/energia_vs_tempo.png')
-show()
+E_min = [20, 30, 40, 50]
 
-title('Plot Energie')
-ylabel('Energia')
-xlabel('# evento')
-plot(energie)
-savefig(immagini + '/energie.png')
-show()
+for i in E_min:
+    energie, tempi = Maxima(timestamp, data, E_min)
 
-title('Plot Tempi')
-ylabel('$\Delta$t')
-xlabel('# evento')
-plot(tempi)
-savefig(immagini + '/tempi.png')
-show()
+    title('Energia vs $\Delta t$')
+    ylabel('Energia')
+    xlabel('$\Delta$ t')
+    scatter(tempi, energie, s=3)
+    savefig(immagini + "/E_min"  + str(i)  + '/energia_vs_tempo.png')
+    show()
 
-title('Istogramma Energie')
-ylabel('Conteggi')
-xlabel('Energia')
-hist(energie, bins='auto')
-savefig(immagini + '/hist_energie.png')
-show()
+    title('Plot Energie')
+    ylabel('Energia')
+    xlabel('# evento')
+    plot(energie)
+    savefig(immagini + "/E_min"  + str(i)  + '/energie.png')
+    show()
 
-title('Istogramma Tempi')
-ylabel('Conteggi')
-xlabel('$\Delta$t')
-hist(tempi, bins='auto')
-savefig(immagini + '/hist_tempi.png')
-show()
+    title('Plot Tempi')
+    ylabel('$\Delta$t')
+    xlabel('# evento')
+    plot(tempi)
+    savefig(immagini + "/E_min"  + str(i)  + '/tempi.png')
+    show()
+
+    title('Istogramma Energie')
+    ylabel('Conteggi')
+    xlabel('Energia')
+    hist(energie, bins='auto')
+    savefig(immagini + "/E_min"  + str(i)  + '/hist_energie.png')
+    show()
+
+    title('Istogramma Tempi')
+    ylabel('Conteggi')
+    xlabel('$\Delta$t')
+    hist(tempi, bins='auto')
+    savefig(immagini + "/E_min"  + str(i)  + '/hist_tempi.png')
+    show()
+
